3b288ca54d6380202bda2522fd7f031df75cef72
Hammad Majid <hammadmajid@proton.me>
2025-09-04 16:53:21 +0500

    add assignment template for PDF generation

--- internal/utils/fs.go
+++ internal/utils/fs.go
@@ -6,8 +6,10 @@ type Template int
 
 const (
 	Index Template = iota
+	Assignment
 )
 
 var TemplateFiles = map[Template]*template.Template{
-	Index: template.Must(template.ParseFiles("web/templates/index.html")),
+	Index:      template.Must(template.ParseFiles("web/templates/index.html")),
+	Assignment: template.Must(template.ParseFiles("web/templates/assignment.html")),
 }
