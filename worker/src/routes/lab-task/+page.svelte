294ef8a03327d0fe36be5361df1739e122e6ceaa
Hammad Majid <hammadmajid@proton.me>
2025-09-10 22:00:15 +0500

    create lab task page

---
+++ worker/src/routes/lab-task/+page.svelte
@@ -0,0 +1,51 @@
+<script lang="ts">
+    import AutoForm from "$lib/components/auto-form.svelte";
+    import DueDate from "$lib/components/due-date.svelte";
+    import Button from "$lib/components/ui/button/button.svelte";
+    import Input from "$lib/components/ui/input/input.svelte";
+    import * as Card from "$lib/components/ui/card/index";
+</script>
+
+<svelte:head>
+    <title>lab task | zabdoc</title>
+</svelte:head>
+
+<div class="space-y-12">
+    <div class="prose">
+        <h1>Lab Task</h1>
+        <p>Fill out the form below to generate lab task in pdf format.</p>
+    </div>
+    <form
+        action="/api/lab-task"
+        method="POST"
+        enctype="multipart/form-data"
+        class="grid md:grid-cols-2 gap-8"
+    >
+        <AutoForm />
+
+        <Card.Root class="">
+            <Card.Header>
+                <Card.Title>Document</Card.Title>
+                <Card.Description
+                    >Information about the lab task.
+                </Card.Description>
+            </Card.Header>
+            <Card.Content class="space-y-4">
+                <DueDate />
+
+                <Input name="number" type="number" placeholder="Number" />
+            </Card.Content>
+        </Card.Root>
+
+        <Card.Root class="">
+            <Card.Header>
+                <Card.Description
+                    >By clicking the button below you accept terms and privacy/
+                </Card.Description>
+            </Card.Header>
+            <Card.CardContent>
+                <Button type="submit" class="w-full">Generate cover</Button>
+            </Card.CardContent>
+        </Card.Root>
+    </form>
+</div>
