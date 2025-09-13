c8fc4d38e4a4947d17ab6ff6bb989d91d50fadcd
Hammad Majid <hammadmajid@proton.me>
2025-09-13 23:27:11 +0500

    fix cors (again)

--- internal/middleware/cors.go
+++ internal/middleware/cors.go
@@ -10,8 +10,8 @@ import (
 var CORSOptions = cors.Options{
 	// Allow requests from localhost:5173 and zabdoc.xyz (including subdomains)
 	AllowedOrigins: []string{
-		"http://localhost:5173",
-		"https://localhost:5173",
+		"http://zabdoc.localhost:5173",
+		"https://zabdoc.localhost:5173",
 		"https://zabdoc.xyz",
 		"https://*.zabdoc.xyz",
 		"http://zabdoc.xyz",
