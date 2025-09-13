54fa9202d947581c6edd71dc40f7e9832a77a835
Hammad Majid <hammadmajid@proton.me>
2025-09-14 00:46:50 +0500

    merge request type into one

--- internal/api/dto/dto.go
+++ internal/api/dto/dto.go
@@ -1,35 +1,25 @@
 package dto
 
-type AssignmentRequest struct {
-	RegNo       string
-	StudentName string
-	Class       string
-	Course      string
-	CourseCode  string
-	Instructor  string
-	Number      string
-	Date        string
-}
-
-type TaskImage struct {
+type SectionImage struct {
 	Data     string // Base64 encoded image data
 	MimeType string // MIME type (e.g., "image/jpeg", "image/png")
 }
 
-type Task struct {
+type Section struct {
 	Index   int
-	Content string      // HTML content (converted from markdown)
-	Images  []TaskImage // Base64 encoded images with MIME types
+	Content string         // HTML content (converted from markdown)
+	Images  []SectionImage // Base64 encoded images with MIME types
 }
 
-type LabTaskRequest struct {
+type GenerateRequest struct {
 	StudentName string
 	RegNo       string
 	Class       string
 	Course      string
 	CourseCode  string
 	Instructor  string
+	DocType     string
 	Number      string
 	Date        string
-	Tasks       []Task
+	Sections    []Section
 }
