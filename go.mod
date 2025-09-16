434c212e78f7f997666fa9615ef2c7d2bc9a2439
Hammad Majid <hammadmajid@proton.me>
2025-09-17 00:28:14 +0500

    circut breaker for pdf service

--- go.mod
+++ go.mod
@@ -12,6 +12,7 @@ require (
 	github.com/aymerick/douceur v0.2.0 // indirect
 	github.com/gorilla/css v1.0.1 // indirect
 	github.com/microcosm-cc/bluemonday v1.0.27 // indirect
+	github.com/sony/gobreaker v1.0.0 // indirect
 	github.com/yuin/goldmark v1.7.13 // indirect
 	golang.org/x/net v0.26.0 // indirect
 )
--- internal/services/pdf_service.go
+++ internal/services/pdf_service.go
@@ -4,17 +4,20 @@ import (
 	"bytes"
 	"context"
 	"os"
+	"time"
 	"zabdoc/internal/api/dto"
 	"zabdoc/internal/templates"
 
 	"github.com/chromedp/cdproto/emulation"
 	"github.com/chromedp/cdproto/page"
 	"github.com/chromedp/chromedp"
+	"github.com/sony/gobreaker"
 )
 
 // PDFService handles PDF generation from HTML.
 type PDFService struct {
 	ctx context.Context
+	cb  *gobreaker.CircuitBreaker
 }
 
 func NewPDFService() *PDFService {
@@ -25,60 +28,80 @@ func NewPDFService() *PDFService {
 	)
 
 	allocCtx, _ := chromedp.NewExecAllocator(context.Background(), opts...)
-
 	ctx, _ := chromedp.NewContext(allocCtx)
+
+	// TODO: send notification to maintainer(s)
+	// if chromedp crashes multiple times then prevent further requests
+	// from going through until the issue is fixed
+	cb := gobreaker.NewCircuitBreaker(gobreaker.Settings{
+		Name:        "chromedp-pdf",
+		MaxRequests: 3,                // max requests allowed in half-open state before tripping again.
+		Interval:    60 * time.Second, // interval to reset internal counts (failures/successes).
+		Timeout:     2 * time.Minute,  // how long to wait before transitioning from open to half-open state.
+		ReadyToTrip: func(counts gobreaker.Counts) bool {
+			// Trip the breaker after 5 consecutive failures.
+			return counts.ConsecutiveFailures >= 5
+		},
+	})
 
 	return &PDFService{
 		ctx: ctx,
+		cb:  cb,
 	}
 }
 
 func (ps *PDFService) GeneratePDF(data dto.GenerateRequest) ([]byte, error) {
-	var buf bytes.Buffer
-	if err := templates.Tpl.Execute(&buf, data); err != nil {
-		return nil, err
-	}
+	result, err := ps.cb.Execute(func() (interface{}, error) {
+		var buf bytes.Buffer
+		if err := templates.Tpl.Execute(&buf, data); err != nil {
+			return nil, err
+		}
 
-	tmpFile, err := os.CreateTemp("", "section_*.html")
+		tmpFile, err := os.CreateTemp("", "section_*.html")
+		if err != nil {
+			return nil, err
+		}
+		defer os.Remove(tmpFile.Name())
+		defer tmpFile.Close()
+
+		if _, err := tmpFile.Write(buf.Bytes()); err != nil {
+			return nil, err
+		}
+		tmpFile.Close()
+
+		fileURL := "file://" + tmpFile.Name()
+
+		var pdfBuf []byte
+
+		err = chromedp.Run(ps.ctx,
+			chromedp.Navigate(fileURL),
+			emulation.SetDeviceMetricsOverride(1280, 1024, 1.0, false),
+			emulation.SetEmulatedMedia(),
+			chromedp.ActionFunc(func(ctx context.Context) error {
+				pdf, _, err := page.PrintToPDF().
+					WithPrintBackground(true).
+					WithScale(1).
+					WithPreferCSSPageSize(true).
+					WithMarginTop(0).
+					WithMarginBottom(0).
+					WithMarginLeft(0).
+					WithMarginRight(0).
+					Do(ctx)
+				if err != nil {
+					return err
+				}
+				pdfBuf = pdf
+				return nil
+			}),
+		)
+		if err != nil {
+			return nil, err
+		}
+
+		return pdfBuf, nil
+	})
 	if err != nil {
 		return nil, err
 	}
-	defer os.Remove(tmpFile.Name())
-	defer tmpFile.Close()
-
-	if _, err := tmpFile.Write(buf.Bytes()); err != nil {
-		return nil, err
-	}
-	tmpFile.Close()
-
-	fileURL := "file://" + tmpFile.Name()
-
-	var pdfBuf []byte
-
-	err = chromedp.Run(ps.ctx,
-		chromedp.Navigate(fileURL),
-		emulation.SetDeviceMetricsOverride(1280, 1024, 1.0, false),
-		emulation.SetEmulatedMedia(),
-		chromedp.ActionFunc(func(ctx context.Context) error {
-			pdf, _, err := page.PrintToPDF().
-				WithPrintBackground(true).
-				WithScale(1).
-				WithPreferCSSPageSize(true).
-				WithMarginTop(0).
-				WithMarginBottom(0).
-				WithMarginLeft(0).
-				WithMarginRight(0).
-				Do(ctx)
-			if err != nil {
-				return err
-			}
-			pdfBuf = pdf
-			return nil
-		}),
-	)
-	if err != nil {
-		return nil, err
-	}
-
-	return pdfBuf, nil
+	return result.([]byte), nil
 }
