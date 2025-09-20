a76261b20976ddfc4561de499c7c6ee153dfc73b
Hammad Majid <hammadmajid@proton.me>
2025-09-20 14:03:55 +0500

    sitemap

---
+++ worker/src/routes/sitemap.xml/+server.ts
@@ -0,0 +1,55 @@
+export async function GET() {
+    return new Response(
+        `
+		<?xml version="1.0" encoding="UTF-8"?>
+        <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+            <url>
+                <loc>https://zabdoc.xyz/</loc>
+                <lastmod>2025-09-20</lastmod>
+                <changefreq>weekly</changefreq>
+                <priority>1.0</priority>
+            </url>
+            <url>
+                <loc>https://zabdoc.xyz/about</loc>
+                <lastmod>2025-09-20</lastmod>
+                <changefreq>weekly</changefreq>
+                <priority>0.8</priority>
+            </url>
+            <url>
+                <loc>https://zabdoc.xyz/assignment</loc>
+                <lastmod>2025-09-20</lastmod>
+                <changefreq>weekly</changefreq>
+                <priority>0.9</priority>
+            </url>
+            <url>
+                <loc>https://zabdoc.xyz/help</loc>
+                <lastmod>2025-09-20</lastmod>
+                <changefreq>weekly</changefreq>
+                <priority>0.7</priority>
+            </url>
+            <url>
+                <loc>https://zabdoc.xyz/lab-project</loc>
+                <lastmod>2025-09-20</lastmod>
+                <changefreq>weekly</changefreq>
+                <priority>0.9</priority>
+            </url>
+            <url>
+                <loc>https://zabdoc.xyz/lab-task</loc>
+                <lastmod>2025-09-20</lastmod>
+                <changefreq>weekly</changefreq>
+                <priority>0.9</priority>
+            </url>
+            <url>
+                <loc>https://zabdoc.xyz/settings</loc>
+                <lastmod>2025-09-20</lastmod>
+                <changefreq>weekly</changefreq>
+                <priority>0.6</priority>
+            </url>
+        </urlset>`.trim(),
+        {
+            headers: {
+                'Content-Type': 'application/xml'
+            }
+        }
+    );
+}
