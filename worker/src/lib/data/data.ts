fb80e440e9861ee44da1c9594cc3abc0e4629cf3
Hammad Majid <hammadmajid@proton.me>
2026-02-14 15:01:02 +0500

    update data for spring 2026

--- worker/src/lib/data/data.ts
+++ worker/src/lib/data/data.ts
@@ -6,53 +6,25 @@ export type CourseInfo = {
 export type ClassData = Record<string, CourseInfo>;
 
 export type DataStructure = {
-    "BsCS-4C": ClassData;
     "BsCS-5C": ClassData;
-    "BsCS-6A": ClassData;
 };
 
 export const data: DataStructure = {
-    "BsCS-4C": {
-        "Database Systems": {
-            instructor: "Muhammad Qasim",
-            code: "CSC 2203",
-        },
-        "Finite Automata Theory and Formal Languages": {
-            instructor: "Taseer Ul Islam",
-            code: "CSC 2204",
-        },
-        "Linear Algebra": {
-            instructor: "Dr. Sajjad Ghouri",
-            code: "CSC 2206",
-        },
-        "Design and Analysis of Algorithms": {
-            instructor: "Aniqa",
-            code: "CSC 3202",
-        },
-        "Management Principle": {
-            instructor: "Dr. Beenish Ambreen",
-            code: "CSC 4603",
-        },
-        "Lab: Database Systems": {
-            instructor: "Mr. Farhan Sami",
-            code: "CSCL 2203",
-        },
-    },
     "BsCS-5C": {
         "Differential Equations": {
-            instructor: "Hamid Qureshi",
+            instructor: "Abdul Moqeet",
             code: "CSC 2122",
         },
         "Graph Theory": {
-            instructor: "Zahoor Shah",
+            instructor: "Kamran Suhaib",
             code: "CSC 2123",
         },
         "Operating Systems": {
-            instructor: "Ghaffar Ahmed",
+            instructor: "Fakhar Ul Islam",
             code: "CSC 2205",
         },
         "Software Engineering": {
-            instructor: "Awais Nawaz",
+            instructor: "Tehreem Saboor",
             code: "CSC 3109",
         },
         "Compiler Construction": {
@@ -60,42 +32,8 @@ export const data: DataStructure = {
             code: "CSC 3201",
         },
         "Lab: Operating Systems": {
-            instructor: "Adeel Ahmed",
+            instructor: "Sohail Ahmad",
             code: "CSCL 2205",
-        },
-    },
-    "BsCS-6A": {
-        "Natural Language Processing": {
-            instructor: "Saad Irfan khan",
-            code: "AIC 3603",
-        },
-        "Technical and Business Writing": {
-            instructor: "Aneela Kanwal",
-            code: "CSC 1205",
-        },
-        "Computer Networks and Data Communications": {
-            instructor: "Rana Faisal Hayat",
-            code: "CSC 3205",
-        },
-        "Artificial Intelligence": {
-            instructor: "Muhammad Qasim",
-            code: "CSC 4101",
-        },
-        "Web Technologies-I": {
-            instructor: "Zubair Ahmed Chatta",
-            code: "CSC 4717",
-        },
-        "Android Application Development": {
-            instructor: "Muhammad Azhar",
-            code: "CSC 4802",
-        },
-        "Lab: Computer Networks and Data Communications": {
-            instructor: "Shafaq Rasheed",
-            code: "CSCL 3205",
-        },
-        "Lab: Artificial Intelligence": {
-            instructor: "Muhammad Usama",
-            code: "CSCL 4101",
         },
     },
 };
