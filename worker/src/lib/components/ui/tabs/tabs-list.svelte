a05049d7930a6fc5bf70dfe05a5d407d4bf30917
Hammad Majid <hammadmajid@proton.me>
2026-03-11 03:52:15 +0500

    redesign tabs UI

--- worker/src/lib/components/ui/tabs/tabs-list.svelte
+++ worker/src/lib/components/ui/tabs/tabs-list.svelte
@@ -13,7 +13,7 @@
 	bind:ref
 	data-slot="tabs-list"
 	class={cn(
-		"bg-muted text-muted-foreground inline-flex h-9 w-fit items-center justify-center rounded-lg p-[3px]",
+		"inline-flex w-full items-center justify-center neo-border-sm bg-card",
 		className
 	)}
 	{...restProps}
