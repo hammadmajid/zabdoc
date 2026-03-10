a1673290f7990677fe03578ff8af6f6bdbb45099
Hammad Majid <hammadmajid@proton.me>
2026-03-11 03:57:03 +0500

    redesign table

--- worker/src/lib/components/ui/table/table-row.svelte
+++ worker/src/lib/components/ui/table/table-row.svelte
@@ -14,7 +14,7 @@
 	bind:this={ref}
 	data-slot="table-row"
 	class={cn(
-		"hover:[&,&>svelte-css-wrapper]:[&>th,td]:bg-muted/50 data-[state=selected]:bg-muted border-b transition-colors",
+		"border-t border-border transition-colors hover:bg-muted/30",
 		className
 	)}
 	{...restProps}
