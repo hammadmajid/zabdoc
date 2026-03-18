d56a148320565003027f07880d558104e7527681
Hammad Majid <hammadmajid@proton.me>
2026-03-19 02:06:39 +0500

    dynamic cors allowed origin using env var

--- internal/middleware/cors.go
+++ internal/middleware/cors.go
@@ -2,23 +2,24 @@ package middleware
 
 import (
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/go-chi/cors"
 )
 
+func getAllowedOrigin() string {
+	origin := os.Getenv("ALLOWED_ORIGIN")
+	if origin == "" {
+		panic("env ALLOWED_ORIGIN is not defined")
+	}
+
+	return origin
+}
+
 var CORSOptions = cors.Options{
 	// Allow requests from localhost:5173 and zabdoc.xyz (including subdomains)
-	AllowedOrigins: []string{
-		"http://localhost:5173",
-		"https://localhost:5173",
-		"http://zabdoc.localhost:5173",
-		"https://zabdoc.localhost:5173",
-		"https://zabdoc.xyz",
-		"https://*.zabdoc.xyz",
-		"http://zabdoc.xyz",
-		"http://*.zabdoc.xyz",
-	},
+	AllowedOrigins: []string{getAllowedOrigin()},
 
 	// Allow all HTTP methods
 	AllowedMethods: []string{
