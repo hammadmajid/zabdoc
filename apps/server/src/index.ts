519fbc4a7fa072cd56ab19e2aee7f96447d5e05a
Hammad Majid <hammadmajid@proton.me>
2026-03-28 17:51:35 +0500

    chore format code

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,12 +1,8 @@
 import { App } from "./app";
-export { ZabdocContainer } from "./container"
+export { ZabdocContainer } from "./container";
 
 export default {
-    async fetch(
-        request: Request,
-        env: Env,
-        ctx: ExecutionContext,
-    ): Promise<Response> {
-        return App.fetch(request, env, ctx);
-    },
+  async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+    return App.fetch(request, env, ctx);
+  },
 };
