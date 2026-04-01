7868c7b3cf3ccf2738d55b21db9257974b50a98b
Hammad Majid <hammadmajid@proton.me>
2026-04-01 13:51:43 +0500

    add packages/types

---
+++ packages/types/src/index.ts
@@ -0,0 +1,47 @@
+import { z } from "zod";
+
+export const documentSchema = z.object({
+    Students: z.array(z.object({
+        Name: z.string().min(1),
+        RegNo: z.string().min(1),
+    })),
+    Class: z.string().min(1),
+    Course: z.string().min(1),
+    CourseCode: z.string().min(1),
+    Instructor: z.string().min(1),
+    DocType: z.string().min(1),
+    Number: z.string().min(1),
+    Date: z.string().min(1),
+    Marks: z.string().min(1),
+});
+
+export type DocumentSchema = z.infer<typeof documentSchema>;
+
+export const scrapeSchema = z.object({
+    username: z.string().min(1),
+    password: z.string().min(1),
+});
+
+export type ScrapeSchema = z.infer<typeof scrapeSchema>;
+
+export const scrapedData = z.object({
+    username: z.string(),
+    name: z.string(),
+    cgpa: z.number(),
+    courses: z.array(z.object({
+        instructor: z.string(),
+        credits: z.number(),
+        attendence: z.object({
+            lecture: z.number(),
+            date: z.date(),
+            status: z.string(),
+        }),
+        marks: z.object({
+            head: z.string(),
+            total: z.number(),
+            obtained: z.number().optional() // optional means "Not entered"
+        })
+    }))
+});
+
+export type ScrapedData = z.infer<typeof scrapedData>;
